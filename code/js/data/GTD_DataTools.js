223830ef81c4014e1d0949488d6d0bf5ac9c51aa
akrck02 <aketza.vazquez@gmail.com>
2021-05-01 21:47:51 +0200

    DataTools: Actually, JSON forEach is a thing. [Fixes #9]

--- code/js/data/GTD_DataTools.js
+++ code/js/data/GTD_DataTools.js
@@ -1,18 +1,30 @@
-//@DEPENDENCIES
-//none.
-
 /**
- * Returns if something is empty
- * @param {*} e - The element 
- * @returns 
+ * Return if an object is empty
+ * @param {*} object - The object to evaluate
+ * @returns true or false
  */
- export const isEmpty = (object) => {
+export const isEmpty = (object) => {
 
-    if (object == null)       return true;
-    if (object == undefined)  return true;
-    if (object == "")         return true;
-    if (object.length == 0)   return true;
-  
-    return false;
+  if (object == null)       return true;
+  if (object == undefined)  return true;
+  if (object == "")         return true;
+  if (object.length == 0)   return true;
+
+  return false;
+}
+
+export const exists = (object) => {
+  return !isEmpty(object);
+}
+
+export const orElse = (object, value) => {
+  return isEmpty(object) ? value : object;
+}
+
+export const jsonForEach = (object , fn) => {
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      fn(object[key]);
+    }
   }
-  
+}
