3efa16f4ebbfe3b91a707a248843adf6b9110a5f
akrck02 <aketza.vazquez@gmail.com>
2021-05-01 21:55:26 +0200

    Core: making development secure in JS with ENUMS

---
+++ code/js/GTD_Core.js
@@ -0,0 +1,14 @@
+export const EVENTS = {
+    CLICK : "click",
+    FOCUS : "focus",
+    FOCUS_IN : "focusin",
+    FOCUS_OUT : "focusout",
+    MOUSE_OVER : "mouseover",
+    MOUSE_OUT : "mouseout",
+    SCROLL : "scroll",
+    TYPE : "type",
+    INPUT : "input",
+    KEY_PRESS : "keypress",
+    LOAD : "load",
+}
+
